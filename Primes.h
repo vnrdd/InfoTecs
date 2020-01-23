58eb35f4d4a01483e1472ba4a70f9bafcdd54974
vnrdd <56083351+vnrdd@users.noreply.github.com>
2020-01-23 21:07:46 +0300

    Primes_privatefield
    Added the Primes private field including std::vector<lli> nums where we gonna store the array of numbers.

---
+++ Primes.h
@@ -0,0 +1,12 @@
+#ifndef PRIMES_H
+#define PRIMES_H
+#include <vector>
+#include "ordinaryFunctions.h"
+typedef long long int lli;
+
+class Primes {
+   private:
+   std::vector<lli> nums;
+};
+
+#endif  //PRIMES_H
