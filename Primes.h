34c1647f3ab14b83ea3525f685be4e791ea5a29c
vnrdd <56083351+vnrdd@users.noreply.github.com>
2020-01-24 01:33:10 +0300

    Primes_upd
    I've decided to use "set" instead of "vector". Then the "size" method and operator[] have been overloaded.

--- Primes.h
+++ Primes.h
@@ -2,25 +2,38 @@
 #define PRIMES_H
 #include<stdint.h>
 #include<istream>
-#include <vector>
+#include <set>
 #include "ordinaryFunctions.h"
 
 class Primes {
    private:
-    std::vector<uint64_t> nums;
+    std::set<uint64_t> nums;
 
    public:
     Primes(uint64_t max_t){
         for(size_t i = 0; i < max_t; ++i){
-            if(i!= 0 && (i == 1 || isPrime(i, 2))) nums.push_back(i);
+            if(i != 0 && (i == 1 || isPrime(i, 2))) nums.insert(i);
         }
+    };
+
+    uint64_t size(){
+        return nums.size();
+    };
+
+    uint64_t operator[](int index){
+        auto it = nums.begin();
+        for(size_t i = 0; i < index; ++i){
+            it++;
+        }
+        uint64_t el = *it;
+        return el;
     };
 
     friend std::ostream &operator<<(std::ostream &out, const Primes &p){
         for(auto it = p.nums.begin(); it != p.nums.end(); ++it){
             std::cout << *it << " ";
         }
-    }
+    };
 };
 
 #endif  //PRIMES_H
