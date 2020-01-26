409c947d984933f97858978d1c4e7d0a05010390
vnrdd <rud_ivan@bk.ru>
2020-01-26 13:42:54 +0300

    Primes_upd(iterator)
    Added class iterator, overload insert using iterator. Reworked constructor.

--- Primes.h
+++ Primes.h
@@ -8,15 +8,51 @@
 class Primes {
    private:
     std::set<uint64_t> nums;
+    uint64_t max;
 
    public:
+    class iterator : std::set<uint64_t>::iterator {
+       public:
+        std::set<uint64_t>::iterator iter;
+        iterator &operator++() {
+            iter++;
+            return *this;
+        };
+    };
+
+    /*Primes::iterator begin() {
+        iterator it;
+        auto beg = nums.begin();
+        it.iter = beg;
+        return it;
+    };
+
+    Primes::iterator end() {
+        iterator it;
+        auto en = nums.end();
+        it.iter = en;
+        return it;
+    };*/
+
+    std::pair<Primes::iterator, bool> insert(uint64_t value) {
+        iterator it;
+        it.iter = nums.begin();
+        if (max == 0 || isPrime(value, 2) == 0) return std::make_pair(it, 0);
+        nums.insert(value);
+        it.iter = nums.find(value);
+        return std::make_pair(it, 1);
+    }
+
     Primes(uint64_t max_t) {
-        for (size_t i = 0; i < max_t; ++i) {
-            if (i != 0 && (i == 1 || isPrime(i, 2))) nums.insert(i);
+        max = max_t;
+        for (size_t i = 1; i < max_t; ++i) {
+            insert(i);
         }
     };
 
-    Primes(){};
+    Primes() {
+        max = 0;
+    };
 
     uint64_t size() {
         return nums.size();
