26aca67af293ce6d42ecf106c5513ebf85a2c8b6
vnrdd <rud_ivan@bk.ru>
2020-02-05 13:27:46 +0300

    Find reworked

--- Primes.h
+++ Primes.h
@@ -1,9 +1,10 @@
 #ifndef PRIMES_H
 #define PRIMES_H
 #include <stdint.h>
-#include <istream>
 #include <fstream>
+#include <istream>
 #include <set>
+#include <stdexcept>
 #include "ordinaryFunctions.h"
 
 /*!
@@ -12,26 +13,25 @@
 
 class Primes {
    private:
-    std::set<uint64_t> nums;
-    uint64_t max;
+    std::set<uint32_t> nums;
+    uint32_t max;
 
    public:
     /*! Begin iterator */
-    std::set<uint64_t>::iterator begin() {
+    std::set<uint32_t>::iterator begin() {
         return nums.begin();
     }
     /*! End iterator */
-    std::set<uint64_t>::iterator end() {
+    std::set<uint32_t>::iterator end() {
         return nums.end();
     }
 
     /*! Method for pushing values into the container */
-    std::pair<std::set<uint64_t>::iterator, bool> insert(uint64_t value) {
+    std::pair<std::set<uint32_t>::iterator, bool> insert(uint32_t value) {
         if (max == 0 && isPrime(value, 2) == 1) {
             nums.insert(value);
             return std::make_pair(nums.find(value), 1);
-        }
-        else if(max > 0 && value <= max && isPrime(value, 2) == 1){
+        } else if (max > 0 && value <= max && isPrime(value, 2) == 1) {
             nums.insert(value);
             return std::make_pair(nums.find(value), 1);
         }
@@ -39,12 +39,12 @@ class Primes {
     }
 
     /*! Method for popping values from the container */
-    size_t erase(const uint64_t &value){
+    size_t erase(const uint32_t &value) {
         return nums.erase(value);
     }
 
     /*! Upborder container constructor */
-    Primes(uint64_t max_t) {
+    Primes(uint32_t max_t) {
         max = max_t;
         for (size_t i = 1; i <= max_t; ++i) {
             insert(i);
@@ -56,51 +56,56 @@ class Primes {
         max = 0;
     };
 
+    bool operator==(std::set<uint32_t> &s) {
+        if (nums == s) return 1;
+        return 0;
+    }
+
     /*! Returning value is the container size */
-    uint64_t size() {
+    uint32_t size() {
         return nums.size();
     };
 
     /*! [] Operator for taking values */
-    uint64_t operator[](uint64_t index) {
-        if(index < 0 || index > size() - 1) throw myException();
+    uint32_t operator[](uint32_t index) {
+        if (index < 0 || index > size() - 1) throw std::exception();
         auto it = nums.begin();
         for (size_t i = 0; i < index; ++i) {
             it++;
         }
-        uint64_t el = *it;
+        uint32_t el = *it;
         return el;
     };
 
     /*! Method for outputting our container into the file (flags exist: 1-full; 2-superprimes; 3-sophie primes) */
-    void fileOutput(const char *filename, int flag){
+    void fileOutput(const char *filename, int flag) {
         std::ofstream f;
         f.open(filename);
-        if(flag == 0){
-            for(auto it = begin(); it != end(); ++it){
+        if (flag == 0) {
+            for (auto it = begin(); it != end(); ++it) {
                 f << *it << "\n";
             }
         }
-        if(flag == 1){
-            auto isSuper = [](uint64_t value, uint64_t index){
-                if(isPrime(value, 2) && isPrime(index, 2)) return 1;
+        if (flag == 1) {
+            auto isSuper = [](uint32_t value, uint32_t index) {
+                if (isPrime(value, 2) && isPrime(index, 2)) return 1;
                 return 0;
             };
 
             int i = 0;
-            for(auto it = begin(); it != end(); ++it){
-                if(isSuper(*it, i+1)) f << *it << "\n";
+            for (auto it = begin(); it != end(); ++it) {
+                if (isSuper(*it, i + 1)) f << *it << "\n";
                 i++;
             }
         }
-        if(flag == 2){
-            auto isSophie = [](uint64_t value){
-                if(isPrime(value, 2) && isPrime(2*value+1, 2)) return 1;
+        if (flag == 2) {
+            auto isSophie = [](uint32_t value) {
+                if (isPrime(value, 2) && isPrime(2 * value + 1, 2)) return 1;
                 return 0;
             };
 
-            for(auto it = begin(); it != end(); ++it){
-                if(isSophie(*it)){
+            for (auto it = begin(); it != end(); ++it) {
+                if (isSophie(*it)) {
                     f << *it << "\n";
                 }
             }
@@ -109,51 +114,64 @@ class Primes {
     }
 
     /*! Method for outputting our container on the display (flags exist: 1-full; 2-superprimes; 3-sophie primes) */
-    void displayOutput(int flag){
-        if(flag == 1){
-            auto isSuper = [](uint64_t value, uint64_t index){
-                if(isPrime(value, 2) && isPrime(index, 2)) return 1;
+    void displayOutput(int flag) {
+        if (flag == 1) {
+            auto isSuper = [](uint32_t value, uint32_t index) {
+                if (isPrime(value, 2) && isPrime(index, 2)) return 1;
                 return 0;
             };
 
             int i = 0;
-            for(auto it = begin(); it != end(); ++it){
-                if(isSuper(*it, i+1)) std::cout << *it << " ";
+            for (auto it = begin(); it != end(); ++it) {
+                if (isSuper(*it, i + 1)) std::cout << *it << " ";
                 i++;
             }
         }
-        if(flag == 2){
-            auto isSophie = [](uint64_t value){
-                if(isPrime(value, 2) && isPrime(2*value+1, 2)) return 1;
+        if (flag == 2) {
+            auto isSophie = [](uint32_t value) {
+                if (isPrime(value, 2) && isPrime(2 * value + 1, 2)) return 1;
                 return 0;
             };
 
-            for(auto it = begin(); it != end(); ++it){
-                if(isSophie(*it)) std::cout << *it << " ";
+            for (auto it = begin(); it != end(); ++it) {
+                if (isSophie(*it)) std::cout << *it << " ";
             }
         }
     }
 
     /*! Copy constructor */
-    Primes(const Primes& copy){
+    Primes(const Primes &copy) {
         max = copy.max;
         nums = copy.nums;
-    } 
+    }
+
+    void find() {
+        clear();
+        for (int i = 0; i < 100; ++i) {
+            if (isPrime(i, 2)) insert(i);
+        }
+    }
 
     /*! Method for finding and memorizing the sequence */
-    void find(uint64_t count_or_max, std::set<uint64_t> &p2, bool flag){
-        if(size() == 0) throw myException();
-        if(count_or_max == 0) return;
-        if(flag == 0){
-            for(auto it = begin(); it != end(); ++it){
-                if(*it <= count_or_max) p2.insert(*it);
+    void find(uint64_t count_or_max, bool flag) {
+        if (flag == 0 && count_or_max < 2) return;
+        if (flag == 0) {
+            clear();
+            for (int i = 0; i < count_or_max; ++i) {
+                if (isPrime(i, 2)) insert(i);
             }
             return;
         }
-        auto it = begin();
-        if(flag == 1){
-            for(int i = 0; i < count_or_max; ++i){
-                p2.insert(*it); ++it;
+
+        if (flag == 1) {
+            clear();
+            int j = 0;
+            for (int i = 0; i < count_or_max;) {
+                if (isPrime(j, 2)) {
+                    insert(j);
+                    i++;
+                }
+                j++;
             }
             return;
         }
@@ -166,6 +184,12 @@ class Primes {
         }
         return out;
     };
+
+    /*! Method Clear */
+    void clear() {
+        nums.clear();
+        max = 0;
+    }
 
     /*! Destructor */
     ~Primes(){};
