8214907333ed8665bd5c3b54aa390200e6f82ecf
vnrdd <rud_ivan@bk.ru>
2020-02-05 14:30:06 +0300

    upd

--- ordinaryFunctions.h
+++ ordinaryFunctions.h
@@ -1,9 +1,37 @@
 #ifndef ORDINARYFUNCTIONS_H
 #define ORDINARYFUNCTIONS_H
-#include<iostream>
+#include <iostream>
+
+bool checkFile(std::string filename) {
+    int i = 0;
+
+    while (filename[i] != '\0') {
+        if (filename[i] == '.') {
+            if (filename[i + 1] == 't') {
+                if (filename[i + 2] == 'x') {
+                    if (filename[i + 3] == 't') {
+                        if (filename[i + 4] == '\0') {
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+        i++;
+    }
+    return 0;
+}
 
 int isPrime(int a, int i) {
+    if (a == 0 || a == 1) return 0;
     return i * i <= a ? (a % i) && isPrime(a, i + 1) : 1;
 }
 
-#endif //ORDINARYFUNCTIONS_H
+std::ostream &operator<<(std::ostream &out, std::set<uint64_t> &p) {
+    for (auto it = p.begin(); it != p.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    return out;
+}
+
+#endif  //ORDINARYFUNCTIONS_H
