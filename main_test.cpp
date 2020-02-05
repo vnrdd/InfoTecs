8148e5460461a1e8040f903f3f1499cec63da6c4
vnrdd <rud_ivan@bk.ru>
2020-02-05 21:17:13 +0300

    Tests reworked
    Detalized

--- main_test.cpp
+++ main_test.cpp
@@ -1,114 +1,196 @@
 #include "Primes.h"
 
+template <typename T>
+int EQUAL_AND_PRINT(T frst, int second) {
+    if (frst == second) {
+        std::cout << "PASSED";
+        return 1;
+    }
+    std::cout << "FAILED";
+    return 0;
+}
+
+template <typename T>
+int EQUAL_AND_PRINT(T frst, T second) {
+    if (frst == second) {
+        std::cout << "PASSED";
+        return 1;
+    }
+    std::cout << "FAILED";
+    return 0;
+}
+
 int EMPTY_CONSTRUCTOR_TEST() {
+    std::cout << "\n~~~ EMPTY CONSTRUCTOR TEST ~~~\n";
     Primes p;
-    return 0 == p.size();
+    std::cout << "Test 1/1 - ";
+    return EQUAL_AND_PRINT(p.size(), 0);
 }
 
 int BORDER_CONSTRUCTOR_TEST() {
+    std::cout << "\n\n~~~ BORDED CONSTRUCTOR TEST ~~~\n";
     int successes = 0;
     Primes p1(10);
-    successes += (p1.size() == 4);
+    std::cout << "Test 1/5 - ";
+    successes += EQUAL_AND_PRINT(p1.size(), 4);
+
     Primes p2(20);
-    successes += (p2.size() == 8);
+    std::cout << "\nTest 2/5 - ";
+    successes += EQUAL_AND_PRINT(p2.size(), 8);
+
     Primes p3(30);
-    successes += (p3.size() == 10);
+    std::cout << "\nTest 3/5 - ";
+    successes += EQUAL_AND_PRINT(p3.size(), 10);
+
     Primes p4(40);
-    successes += (p4.size() == 12);
+    std::cout << "\nTest 4/5 - ";
+    successes += EQUAL_AND_PRINT(p4.size(), 12);
     Primes p5(50);
-    successes += (p5.size() == 15);
+    std::cout << "\nTest 5/5 - ";
+    successes += EQUAL_AND_PRINT(p5.size(), 15);
     return successes;
 }
 
 int SIZE_TEST() {
+    std::cout << "\n\n~~~ SIZE TEST ~~~\n";
     int successes = 0;
     Primes p;
+
+    std::cout << "Test 1/5 - ";
     p.insert(2);
-    successes += (p.size() == 1);
+    successes += EQUAL_AND_PRINT(p.size(), 1);
 
+    std::cout << "\nTest 2/5 - ";
     p.insert(4);
-    successes += (p.size() == 1);
+    successes += EQUAL_AND_PRINT(p.size(), 1);
 
+    std::cout << "\nTest 3/5 - ";
     p.insert(5);
     p.insert(3);
-    successes += (p.size() == 3);
+    successes += EQUAL_AND_PRINT(p.size(), 3);
 
+    std::cout << "\nTest 4/5 - ";
     p.erase(5);
-    successes += (p.size() == 2);
+    successes += EQUAL_AND_PRINT(p.size(), 2);
 
+    std::cout << "\nTest 5/5 - ";
     p.erase(0);
-    successes += (p.size() == 2);
+    successes += EQUAL_AND_PRINT(p.size(), 2);
+
     return successes;
 }
 
 int INSERT_TEST() {
+    std::cout << "\n\n~~~ INSERT TEST ~~~\n";
     int successes = 0;
     Primes p;
+
+    std::cout << "Test 1/5 - ";
     p.insert(2);
-    successes += (p[0] == 2);
+    successes += EQUAL_AND_PRINT(p[0], 2);
+
+    std::cout << "\nTest 2/5 - ";
     p.insert(3);
-    successes += (p[1] == 3);
+    successes += EQUAL_AND_PRINT(p[1], 3);
+
+    std::cout << "\nTest 3/5 - ";
     p.insert(7);
-    successes += (p[2] == 7);
+    successes += EQUAL_AND_PRINT(p[2], 7);
+
+    std::cout << "\nTest 4/5 - ";
     p.insert(11);
-    successes += (p[3] == 11);
+    successes += EQUAL_AND_PRINT(p[3], 11);
+
+    std::cout << "\nTest 5/5 - ";
     p.insert(13);
-    successes += (p[4] == 13);
+    successes += EQUAL_AND_PRINT(p[4], 13);
+
     return successes;
 }
 
 int ERASE_TEST() {
+    std::cout << "\n\n~~~ ERASE TEST ~~~\n";
     int successes = 0;
     Primes p(20);
+
+    std::cout << "Test 1/5 - ";
     p.erase(2);
-    successes += (p.size() == 7);
+    successes += EQUAL_AND_PRINT(p.size(), 7);
+
+    std::cout << "\nTest 2/5 - ";
     p.erase(5);
-    successes += (p.size() == 6);
+    successes += EQUAL_AND_PRINT(p.size(), 6);
+
+    std::cout << "\nTest 3/5 - ";
     p.erase(4);
-    successes += (p.size() == 6);
+    successes += EQUAL_AND_PRINT(p.size(), 6);
+
+    std::cout << "\nTest 4/5 - ";
     p.erase(7);
-    successes += (p.size() == 5);
+    successes += EQUAL_AND_PRINT(p.size(), 5);
+
+    std::cout << "\nTest 5/5 - ";
     p.erase(10);
-    successes += (p.size() == 5);
+    successes += EQUAL_AND_PRINT(p.size(), 5);
+
     return successes;
 }
 
 int FIND_TEST() {
+    std::cout << "\n\n~~~ FIND TEST ~~~\n";
     int successes = 0;
     Primes p(100);
     Primes test(10);
-    p.find(10, 0);
-    successes += (p == test);
 
+    std::cout << "Test 1/5 - ";
+    p.find(10, 0);
+    successes += EQUAL_AND_PRINT(p, test);
+
+    std::cout << "\nTest 2/5 - ";
     new (&test) Primes(100);
     p.find();
-    successes += (p == test);
+    successes += EQUAL_AND_PRINT(p, test);
 
+    std::cout << "\nTest 3/5 - ";
     new (&test) Primes(40);
     p.find(12, 1);
-    successes += (p == test);
+    successes += EQUAL_AND_PRINT(p, test);
 
+    std::cout << "\nTest 4/5 - ";
     new (&test) Primes;
     p.find(0, 1);
-    successes += (p == test);
+    successes += EQUAL_AND_PRINT(p, test);
 
+    std::cout << "\nTest 5/5 - ";
     p.find(0, 0);
-    successes += (p == test);
+    successes += EQUAL_AND_PRINT(p, test);
     return successes;
 }
 
 int BRACKET_OPERATOR_TEST() {
+    std::cout << "\n\n~~~ BRACKET OPERATOR TEST ~~~\n";
     int successes = 0;
     Primes p(30);
+
+    std::cout << "Test 1/5 - ";
     try {
         p[-1];
     } catch (std::exception &ex) {
+        std::cout << "PASSED";
         successes += 1;
     }
-    successes += (p[3] == 7);
-    successes += (p[0] == 2);
-    successes += (p[2] == 5);
-    successes += (p[7] == 19);
+
+    std::cout << "\nTest 5/5 - ";
+    successes += EQUAL_AND_PRINT(p[3], 7);
+
+    std::cout << "\nTest 5/5 - ";
+    successes += EQUAL_AND_PRINT(p[0], 2);
+
+    std::cout << "\nTest 5/5 - ";
+    successes += EQUAL_AND_PRINT(p[2], 5);
+
+    std::cout << "\nTest 5/5 - ";
+    successes += EQUAL_AND_PRINT(p[7], 19);
     return successes;
 }
 
@@ -122,7 +204,10 @@ void RUN_ALL_TESTS() {
     successes += FIND_TEST();
     successes += BRACKET_OPERATOR_TEST();
     double precentage = successes / 31 * 100;
-    std::cout << "\nResult of all tests: " << successes << "/31 (" << (int)precentage << "%)\n\n";
+
+    std::cout << "\n\n/////////////////////////////////////////\n";
+    std::cout << "Result of all tests: " << successes << "/31 (" << (int)precentage << "%)\n";
+    std::cout << "/////////////////////////////////////////\n\n";
 }
 
 int main(int argc, char **argv) {
